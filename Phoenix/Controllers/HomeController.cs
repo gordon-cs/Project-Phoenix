7ce62c0b382eb5760191d18b2d0316a409a99edd
superpowers11 <stephanie.powers@gordon.edu>
2016-12-03 21:37:44 -0500

    Update HomeController.cs

--- Phoenix/Controllers/HomeController.cs
+++ Phoenix/Controllers/HomeController.cs
@@ -12,10 +12,6 @@ namespace Phoenix.Controllers
     {
         public ActionResult Index()
         {
-            var _context = new RCIContext();
-            var room = new Room { RoomID = "1", Capacity = 2 };
-            _context.Room.Add(room);
-            _context.SaveChanges();
 
             return View();
         }
