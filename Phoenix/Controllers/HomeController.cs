fc81ad2fde363e4fd9fa06df13ed1c6481600885
rachelyou <rachel_you@outlook.com>
2017-01-07 11:06:58 -0500

    Add My RCIs Page

---
+++ Phoenix/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+using Phoenix.Models;
+
+namespace Phoenix.Controllers
+{
+    public class HomeController : Controller
+    {
+        // RCI context wrapper. It can be considered to be an object that represents the database.
+        private RCIContext db;
+
+        public HomeController()
+        {
+            db = new Models.RCIContext();
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home/MyRCIs
+        public ActionResult MyRCIs()
+        {
+            var resRCI = db.ResidentRCI.Where(s => s.ResidentAccountID.Equals("50153295"));
+
+            return View(resRCI);
+        }
+    }
+}
