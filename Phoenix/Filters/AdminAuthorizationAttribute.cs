38a577134d82dbec640fe4a9b3f573c61c6243bf
superpowers11 <stephanie.powers@gordon.edu>
2017-01-23 12:40:12 -0500

    Started authorization filter

---
+++ Phoenix/Filters/AdminAuthorizationAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Web.Mvc.Filters;
+using System.Diagnostics;
+
+namespace Phoenix.Filters
+{
+    public class AdminAuthorizationAttribute : ActionFilterAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+           
+        }
+
+    }
+}
