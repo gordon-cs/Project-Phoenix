fb27676f9b6bedcf2acbea1e99c97774016d63b0
Eze Anyanwu <ezeanyinabia.anyanwu@gordon.edu>
2017-01-23 00:52:56 -0500

    Remove unecessary usings.

--- Phoenix/Controllers/PlaceholderController.cs
+++ Phoenix/Controllers/PlaceholderController.cs
@@ -1,11 +1,4 @@
-﻿using Phoenix.Filters;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Security.Claims;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Phoenix.Controllers
 {
