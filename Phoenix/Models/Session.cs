038f28d9d7fbcde4dea3b65f0b56d024e9cac4b1
superpowers11 <stephanie.powers@gordon.edu>
2017-01-27 17:07:49 -0500

    Removed all unnecessary usings

--- Phoenix/Models/Session.cs
+++ Phoenix/Models/Session.cs
@@ -1,10 +1,8 @@
 namespace Phoenix.Models
 {
     using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("Session")]
     public partial class Session
