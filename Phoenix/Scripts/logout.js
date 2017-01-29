9d3bd757b1b407839f66ca00357e62ecd25ea160
Eze Anyanwu <ezeanyinabia.anyanwu@gordon.edu>
2017-01-29 16:43:30 -0500

    Let user logout

---
+++ Phoenix/Scripts/logout.js
@@ -0,0 +1,14 @@
+﻿/* Logout functionality */
+
+$(".logout-button").click(function () {
+    $.ajax({
+        url: document.location.origin + "/Logout",
+        method: "GET",
+        success: function () {
+            window.location.href = document.location.origin + "/Login";
+        },
+        error: function () {
+            console.log("Error loging out");
+        }
+    });
+});
