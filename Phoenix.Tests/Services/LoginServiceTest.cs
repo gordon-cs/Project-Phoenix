ea3b3711dc8ffa0b44abeb3ef8438dd5c48af4cc
superpowers11 <stephanie.powers@gordon.edu>
2017-01-30 18:12:51 -0500

    Started unit test setup

---
+++ Phoenix.Tests/Services/LoginServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.DirectoryServices;
+using Phoenix.Services;
+
+namespace Phoenix.Tests.Services
+{
+    [TestClass]
+    public class LoginServiceTest
+    {
+        LoginService loginService = new LoginService();
+
+        [TestMethod]
+        public void Given_ADContext_When_ValidUserEntry_Then_ReturnUserEntry()
+        {
+            // Arrange
+            //var ADContext = loginService.ConnectToADServer();
+            
+            // Act
+            // Assert
+        }
+    }
+}
