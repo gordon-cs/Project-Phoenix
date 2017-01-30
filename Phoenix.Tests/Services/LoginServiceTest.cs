dd83ab8ac1987f77f91bba5792a74a9f80c122f8
superpowers11 <stephanie.powers@gordon.edu>
2017-01-30 18:23:42 -0500

    Added first test and started second

--- Phoenix.Tests/Services/LoginServiceTest.cs
+++ Phoenix.Tests/Services/LoginServiceTest.cs
@@ -11,12 +11,27 @@ namespace Phoenix.Tests.Services
         LoginService loginService = new LoginService();
 
         [TestMethod]
-        public void Given_ADContext_When_ValidUserEntry_Then_ReturnUserEntry()
+        public void Given_ADContext_When_ValidUsername_Then_ReturnUserEntry()
+        {
+            // Arrange
+            var ADContext = loginService.ConnectToADServer();
+            var username = "stephanie.powers";
+
+            // Act
+            var result = loginService.FindUser(username, ADContext);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Given_ADContext_When_InvalidUsername_Then_ReturnNull()
         {
             // Arrange
             //var ADContext = loginService.ConnectToADServer();
-            
+
             // Act
+
             // Assert
         }
     }
