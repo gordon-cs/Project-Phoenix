c76e7d4e5ead09aeb9876deae51706335ac93e9c
superpowers11 <stephanie.powers@gordon.edu>
2017-01-30 23:32:45 -0500

    Validation unit tests

--- Phoenix.Tests/Services/LoginServiceTest.cs
+++ Phoenix.Tests/Services/LoginServiceTest.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
 using Phoenix.Services;
 
 namespace Phoenix.Tests.Services
@@ -15,7 +15,7 @@ namespace Phoenix.Tests.Services
         {
             // Arrange
             var ADContext = loginService.ConnectToADServer();
-            var username = "stephanie.powers";
+            var username = "360.StudentTest";
 
             // Act
             var result = loginService.FindUser(username, ADContext);
@@ -28,11 +28,63 @@ namespace Phoenix.Tests.Services
         public void Given_ADContext_When_InvalidUsername_Then_ReturnNull()
         {
             // Arrange
-            //var ADContext = loginService.ConnectToADServer();
+            var ADContext = loginService.ConnectToADServer();
+            var username = "meriadoc.brandybuck";
 
             // Act
+            var result = loginService.FindUser(username, ADContext);
 
             // Assert
+            Assert.IsNull(result);
+        }
+
+        // This test is currently failing, because an exception is thrown when FindUser
+        // gets an invalid ADContext. But I think we can eliminate this test b/c in 
+        // LoginController we check that ADContext is not null beforehand
+        [TestMethod]
+        public void Given_InvalidADContext_When_ValidUsername_Then_ReturnNull()
+        {
+            // Arrange
+            PrincipalContext ADContext = null;
+            var username = "360.StudentTest";
+
+            // Act
+            var result = loginService.FindUser(username, ADContext);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void Given_ADContext_and_ValidUserEntry_When_ValidPassword_Then_ReturnNull()
+        {
+            // Arrange
+            var ADContext = loginService.ConnectToADServer();
+            var username = "360.StudentTest";
+            var password = "Gordon16";
+            var userEntry = loginService.FindUser(username, ADContext);
+
+            // Act
+            bool result = loginService.IsValidUser(username, password, ADContext);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Given_ADContext_and_ValidUserEntry_When_InValidPassword_Then_ReturnNull()
+        {
+            // Arrange
+            var ADContext = loginService.ConnectToADServer();
+            var username = "360.StudentTest";
+            var password = "wrongGordon16";
+            var userEntry = loginService.FindUser(username, ADContext);
+
+            // Act
+            bool result = loginService.IsValidUser(username, password, ADContext);
+
+            // Assert
+            Assert.IsFalse(result);
         }
     }
 }
