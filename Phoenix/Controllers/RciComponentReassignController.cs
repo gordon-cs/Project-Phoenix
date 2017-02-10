41ec9f6c7d95c891597a67a0a355fdbbdceaf15e
Eze Anyanwu <ezeanyinabia.anyanwu@gordon.edu>
2017-02-09 23:55:13 -0500

    controller, service and viewmodels

---
+++ Phoenix/Controllers/RciComponentReassignController.cs
@@ -0,0 +1,37 @@
+﻿using Phoenix.Filters;
+using Phoenix.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Phoenix.Controllers
+{
+    [CustomAuthentication]
+    public class RciComponentReassignController : Controller
+    {
+
+        private RciComponentReassignService reassignService;
+
+        public RciComponentReassignController()
+        {
+            reassignService = new RciComponentReassignService();
+        }
+
+        // GET: RciComponentReassign
+        [HttpGet]
+        public ActionResult Index(int id)
+        {
+            var rci = reassignService.GetRciByID(id);
+            return View(rci);
+        }
+
+        [HttpPost]
+        public ActionResult Index(int id, int[] rciComponent, int assignTo )
+        {
+            reassignService.AssignComponentsToRci(rciComponent, assignTo);
+            return RedirectToAction(actionName:"Index", controllerName:"Dashboard");
+        }
+    }
+}
---
+++ Phoenix/Services/RciComponentReassignService.cs
@@ -0,0 +1,80 @@
+﻿using Phoenix.Models;
+using Phoenix.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Phoenix.Services
+{
+    public class RciComponentReassignService
+    {
+        private RCIContext db;
+        public RciComponentReassignService()
+        {
+            db = new RCIContext();
+        }
+
+        public RciReassignViewModel GetRciByID(int id)
+        {
+            var temp = db.Rci.Find(id);
+            var gordonID = temp.GordonID;
+            string firstName, lastName;
+
+            if (gordonID != null)
+            {
+                var account = db.Account.Where(m => m.ID_NUM == gordonID).FirstOrDefault();
+                firstName = account.firstname;
+                lastName = account.lastname;
+            }
+            else
+            {
+                firstName = "Common Area";
+                lastName = "RCI";
+            }
+
+            var rci = new RciReassignViewModel()
+            {
+                RciID = temp.RciID,
+                GordonID = gordonID,
+                FirstName = firstName,
+                LastName = lastName,
+                BuildingCode = temp.BuildingCode,
+                RoomNumber = temp.RoomNumber,
+                RciComponent = temp.RciComponent
+            };
+
+            // Find the rcis for the room mates, since those are the only people you can reassign 
+            // components to.
+            var roommateRcis =
+                from r in db.Rci
+                join a in db.Account on r.GordonID equals a.ID_NUM
+                where r.IsCurrent == true &&
+                r.BuildingCode == rci.BuildingCode &&
+                r.RoomNumber == rci.RoomNumber
+                select new PotentialRciReassignTarget
+                {
+                    RciID = r.RciID,
+                    RciOwner = a.firstname + " " + a.lastname
+                };
+
+            rci.RciTarget = roommateRcis.ToList();
+
+            return rci;
+        }
+
+        // Set the RciID column for a list components.
+        public void AssignComponentsToRci(int[] rciComponents, int rciID)
+        {
+            var query = db.RciComponent.Where(m => rciComponents.Contains(m.RciComponentID));
+            foreach(var record in query)
+            {
+                record.RciID = rciID;
+            }
+
+            db.SaveChanges();
+        }
+
+
+    }
+}
