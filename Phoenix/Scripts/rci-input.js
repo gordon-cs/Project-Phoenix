bb57641d0760bfcc87101f3f24aea45c48b8a647
Eze Anyanwu <ezeanyinabia.anyanwu@gordon.edu>
2017-03-19 16:02:39 -0400

    Include the check() method that was forgotten

--- Phoenix/Scripts/rci-input.js
+++ Phoenix/Scripts/rci-input.js
@@ -246,6 +246,34 @@ function RDSigSubmit() {
 }
 
 
+$("#rci-sig-checkbox").click(function () {
+    check();
+});
+
+function check() {
+    if ($("#rci-sig-checkbox").is(":checked")) {
+        var sigCheck = 1;
+    }
+    else {
+        var sigCheck = 0;
+    }
+    var id = $("h2[id^='rci-']").first().attr("id").substring(4);
+    $.ajax({
+        sync: false,
+        url: "/RciInput/CheckSigRD",
+        data: { sigCheck: sigCheck, id: id },
+        method: "POST",
+        /*success: function (data) {
+            window.location.href = data;
+        },*/
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    })
+}
+
+
 /* Add a div to the right component. This div will contain a :
     <p> element displaying a damage
     <input> hidden element that will be used when submitting the form
