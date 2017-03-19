522ec69c1544a68843d1a8254ffb0c84c851cecd
Eze Anyanwu <ezeanyinabia.anyanwu@gordon.edu>
2017-03-19 15:48:24 -0400

    Sign the RA column as well if the logged in RA is the owner of the rci.

--- Phoenix/Services/RciInputService.cs
+++ Phoenix/Services/RciInputService.cs
@@ -103,7 +103,7 @@ namespace Phoenix.Services
         {
             var rci = db.Rci.Where(m => m.RciID == id).FirstOrDefault();
 
-            if (rciSig == user)
+            if (rciSig == user || rci.GordonID == gordonID) // rciSig is null when it is the RA signing for himself. Make another check to see if the logged in RA is the owner of the rci.
             {
                 rci.CheckinSigRA = DateTime.Today;
                 rci.CheckinSigRAGordonID = gordonID;
