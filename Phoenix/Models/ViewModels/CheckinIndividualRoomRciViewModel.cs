9f7ccda59e8a5e642554cbdc23b7f7395e0a91ab
Eze Anyanwu <ezeanyinabia.anyanwu@gordon.edu>
2017-03-31 14:50:08 -0400

    Modify viewmodels to reflect if damages/fines exist.

--- Phoenix/Models/ViewModels/CheckinIndividualRoomRciViewModel.cs
+++ Phoenix/Models/ViewModels/CheckinIndividualRoomRciViewModel.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 
 namespace Phoenix.Models.ViewModels
 {
-    /// <summary>
-    /// Used to store the information that is to be displayed during checkin on the signature page of a common are rci.
-    /// </summary>
-    public class CheckinCommonAreaRciViewModel
+    public class CheckinIndividualRoomRciViewModel
     {
         public int RciID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string BuildingCode { get; set; }
         public string RoomNumber { get; set; }
+        public string GordonID { get; set; }
         public ICollection<RciComponent> RciComponent { get; set; }
-        public ICollection<CommonAreaMember> CommonAreaMember { get; set; }
         public DateTime? CheckinSigRes { get; set; }
         public DateTime? CheckinSigRA { get; set; }
         public DateTime? CheckinSigRD { get; set; }
@@ -25,24 +25,6 @@ namespace Phoenix.Models.ViewModels
         public bool DamagesExist()
         {
             return RciComponent.Where(x => x.Damage.Any()).Any();
-        }
-
-        public bool FinesExist()
-        {
-            return RciComponent.Where(x => x.Fine.Any()).Any();
-        }
-
-        public bool EveryoneHasSigned()
-        {
-            var everyoneHasSigned = true;
-            foreach (var member in CommonAreaMember)
-            {
-                if (member.HasSignedCommonAreaRci == false)
-                {
-                    everyoneHasSigned = false;
-                }
-            }
-            return everyoneHasSigned;
         }
     }
 }
