0658ae0ad1987c7785feaa986206fbb0695f146f
Eze Anyanwu <ezeanyinabia.anyanwu@gordon.edu>
2017-03-31 21:27:13 -0400

    Restrict access to rcis

---
+++ Phoenix/Models/PartialClasses/Rci.cs
@@ -0,0 +1,31 @@
+﻿using Phoenix.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Phoenix.Models
+{
+    public partial class Rci
+    {
+        public bool isViewableBy(string userID, string role, string currentRoomNumber, string currentBuilding)
+        {
+            if (role == Constants.RA || role == Constants.RD || role == Constants.ADMIN)
+            {
+                return true;
+            }
+            else if (GordonID == userID)
+            {
+                return true;
+            }
+            else if (currentRoomNumber.TrimEnd(new char[] { 'A', 'B', 'C', 'D' }) == RoomNumber && currentBuilding == BuildingCode)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
